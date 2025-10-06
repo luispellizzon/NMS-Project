b4ed3b60d6783e6491d32487fcb7a6f9637f6ce4
Luis Pellizzon <luis.pellizzon@gmail.com>
2025-10-06 20:56:03 +0100

    fix(firebase): isolatedModules config fix

--- firebase/functions/src/types/index.ts
+++ firebase/functions/src/types/index.ts
@@ -2,4 +2,4 @@ import { SignInCredentials } from "./signin-credentials";
 import { UserCredentials } from "./user-credentials";
 import { Response } from "./response";
 
-export { SignInCredentials, UserCredentials, Response }
+export type { SignInCredentials, UserCredentials, Response }
